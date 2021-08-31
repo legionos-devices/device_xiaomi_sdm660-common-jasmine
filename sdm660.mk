60bbe978400abdd2b22284e31dfdbde4b96d7b13
IMMANUEL44 <immanuelr44@gmail.com>
2021-08-31 17:30:26 +0530

    Revert "sdm660-common: Add shim for libdpmframework"
    This reverts commit 0d6967b2cff7b2f6bfbeb3c2e29c2748a460fb60.

--- sdm660.mk
+++ sdm660.mk
@@ -542,10 +542,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
 
-# Shims
-PRODUCT_PACKAGES += \
-    libcutils_shim
-
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
