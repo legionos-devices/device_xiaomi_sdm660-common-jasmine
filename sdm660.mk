3e054a7b22009035d7b1c27903ac153a226ddb8f
IMMANUEL44 <immanuelr44@gmail.com>
2021-09-09 18:00:35 +0530

    sdm660-common: Build camera.provider @2.4 & 2.5

--- sdm660.mk
+++ sdm660.mk
@@ -191,6 +191,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5:64 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4:64 \
+    android.hardware.camera.provider@2.5:64 \
     android.hardware.camera.provider@2.6:64 \
     vendor.qti.hardware.camera.device@1.0:64 \
     android.hardware.camera.common@1.0
