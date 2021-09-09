e9ad7466e1dfc0830fc93bb0ea8ff67b4b9e8cb8
IMMANUEL44 <immanuelr44@gmail.com>
2021-09-09 16:59:27 +0530

    sdm660-common: Build audio@4 and 5
    also audio.service
    
    Thes will act as fallback amp incase one fails other takes charge

--- sdm660.mk
+++ sdm660.mk
@@ -94,13 +94,26 @@ $(COMMON_PATH)/configs/lm/AdaptLaunchFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/l
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@4.0 \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.common@4.0 \
+    android.hardware.audio.common@4.0-util \
+    android.hardware.audio.effect@4.0 \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.audio@5.0 \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.common@5.0 \
     android.hardware.audio.common@5.0-util \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@6.0 \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.common@6.0 \
+    android.hardware.audio.common@6.0-util \
+    android.hardware.audio.effect@6.0 \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl:32 \
     audio.a2dp.default \
     audio.r_submix.default \
